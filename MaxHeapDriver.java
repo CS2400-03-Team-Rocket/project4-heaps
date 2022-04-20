42d0d2da960fb6026e5266470edd0006428773fe
Eric Huang <erichuang54@gmail.com>
2022-04-19 22:12:37 -0700

    fixed pathname

--- MaxHeapDriver.java
+++ MaxHeapDriver.java
@@ -6,7 +6,7 @@ import java.io.IOException;;
 public class MaxHeapDriver{
     public static void main(String [] args) throws IOException{
         MaxHeap<String> maxHeap = new MaxHeap<>();
-        File dataRandom = new File("/Users/andre/Desktop/proj4_code/data/data_random.txt");
+        File dataRandom = new File("data/data_random.txt");
         Scanner scan = new Scanner(dataRandom);
         
         while(scan.hasNextLine()){
@@ -18,7 +18,7 @@ public class MaxHeapDriver{
         //add the heap contents to fileContent
         fileContent = maxHeap.toString();
 
-        FileWriter writer = new FileWriter("/Users/andre/Desktop/output.txt");
+        FileWriter writer = new FileWriter("output.txt");
         writer.write(fileContent);
         writer.close();
         System.out.println("number of swaps: " + maxHeap.getSwaps());
