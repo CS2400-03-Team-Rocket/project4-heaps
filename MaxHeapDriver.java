be304a9e89d511caedfaa78f9f4247efee318cd4
Eric Huang <erichuang54@gmail.com>
2022-04-19 23:29:39 -0700

    testing optimal method for maxheap

--- MaxHeapDriver.java
+++ MaxHeapDriver.java
@@ -1,7 +1,8 @@
 import java.io.File;
 import java.util.Scanner;
 import java.io.FileWriter;
-import java.io.IOException;;
+import java.io.IOException;
+import java.io.*;
 
 public class MaxHeapDriver{
     public static void main(String [] args) throws IOException{
@@ -22,5 +23,23 @@ public class MaxHeapDriver{
         writer.write(fileContent);
         writer.close();
         System.out.println("number of swaps: " + maxHeap.getSwaps());
+
+
+        //CREATING HEAP USING OPTIMAL METHOD
+        Scanner scan2 = new Scanner(dataRandom);
+        String[] a = new String[100];
+        int i = 0;
+        while(scan2.hasNextLine()){
+            a[i] = scan2.nextLine();
+            i++;
+        }
+        scan2.close();
+        MaxHeap<String> maxHeapOptimal = new MaxHeap<String>(a);
+        String fileContent2 = maxHeapOptimal.toString();
+
+        FileWriter writer2 = new FileWriter("output2.txt");
+        writer2.write(fileContent2);
+        writer2.close();
+        System.out.println("number of swaps: " + maxHeapOptimal.getSwaps());
     }
 }
